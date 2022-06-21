e548a2bab956b010ad1ce762a6c517e53643ebfc
ArthyrMyagkov <Arthyr16@gmail.com>
2022-06-21 15:34:44 +0300

    ThirdCommit

---
+++ Scripts/Main.js
@@ -0,0 +1,18 @@
+
+let id_myHardSkills="id_myHardSkills"
+let id_mySoftSkills="id_mySoftSkills"
+let id_myContacts = "id_myContacts"
+let id_myWork = "id_myWork"
+let id_myEducation = "id_myEducation"
+let id_AboutMe = "id_AboutMe"
+
+let element=document.getElementById(id_AboutMe);
+
+function removeClass(id_name) {
+    element.classList.add("Displayable-section_none");
+    element=document.getElementById(id_name);
+    element.classList.remove("Displayable-section_none");
+    console.log(element)
+    
+    return element;
+}
